818d8a7edad810982c4d4c4032ff74e94fbdb048
Jung-kr <jungwy98@gmail.com>
2025-12-28 15:12:04 +0900

    chore(board-service): 스프링 부트 프로젝트

---
+++ board-service/src/test/java/woobl0g/boardservice/BoardServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package woobl0g.boardservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
