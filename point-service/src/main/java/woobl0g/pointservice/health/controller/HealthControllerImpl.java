526dcdbc8f49e8842adff30aa0fe92b8fc704338
Jung-kr <jungwy98@gmail.com>
2025-12-28 16:35:10 +0900

    feat(point-service): Swagger 인터페이스 분리 기반 헬스 체크 API 추가

---
+++ point-service/src/main/java/woobl0g/pointservice/health/controller/HealthControllerImpl.java
@@ -0,0 +1,26 @@
+package woobl0g.pointservice.health.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import woobl0g.pointservice.global.exception.BaseException;
+import woobl0g.pointservice.global.response.ApiResponse;
+import woobl0g.pointservice.global.response.ResponseCode;
+
+@RestController
+@RequestMapping("/api/health")
+public class HealthControllerImpl implements HealthController {
+
+    @Override
+    @GetMapping
+    public ResponseEntity<ApiResponse<Void>> healthCheck(String value) {
+        if(value != null) {
+            throw new BaseException(ResponseCode.INVALID_REQUEST);
+        }
+
+        return ResponseEntity
+                .status(ResponseCode.HEALTH_CHECK_SUCCESS.getStatus())
+                .body(ApiResponse.success(ResponseCode.HEALTH_CHECK_SUCCESS));
+    }
+}
