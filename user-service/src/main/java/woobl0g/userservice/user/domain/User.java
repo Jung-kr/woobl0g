1c00807dfb6d81f3560347568bae786a6ecf4757
Jung-kr <jungwy98@gmail.com>
2025-12-29 20:02:49 +0900

    feat(user-service): User 도메인 엔티티 생성

---
+++ user-service/src/main/java/woobl0g/userservice/user/domain/User.java
@@ -0,0 +1,34 @@
+package woobl0g.userservice.user.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+    private String email;
+    private String name;
+    private String password;
+    private LocalDateTime createdAt;
+
+    private User(String email, String name, String password) {
+        this.email = email;
+        this.name = name;
+        this.password = password;
+        this.createdAt = LocalDateTime.now();
+    }
+
+    public static User create(String email, String name, String password) {
+        return new User(email, name, password);
+    }
+}
