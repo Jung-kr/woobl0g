842fb4a8446007593bc1df4b873bcd5472bb5366
Jung-kr <jungwy98@gmail.com>
2026-01-08 15:31:57 +0900

    feat(point-service): 포인트 조회 API에서 PathVariable 대신 X-User-Id 헤더 사용 및 관리자 포인트 적립 & 차감 API 구현

--- point-service/src/main/java/woobl0g/pointservice/point/controller/PointControllerImpl.java
+++ point-service/src/main/java/woobl0g/pointservice/point/controller/PointControllerImpl.java
@@ -5,10 +5,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import woobl0g.pointservice.global.response.ApiResponse;
 import woobl0g.pointservice.global.response.ResponseCode;
 import woobl0g.pointservice.point.dto.PointHistoryResponseDto;
@@ -25,8 +22,8 @@ public class PointControllerImpl implements PointController {
     private final PointService pointService;
 
     @Override
-    @GetMapping("/{userId}/history")
-    public ResponseEntity<ApiResponse<List<PointHistoryResponseDto>>> getPointHistory(@PathVariable Long userId) {
+    @GetMapping("/history")
+    public ResponseEntity<ApiResponse<List<PointHistoryResponseDto>>> getPointHistory(@RequestHeader("X-User-Id") Long userId) {
         return ResponseEntity
                 .status(ResponseCode.POINT_HISTORY_GET_SUCCESS.getStatus())
                 .body(ApiResponse.success(ResponseCode.POINT_HISTORY_GET_SUCCESS, pointService.getPointHistory(userId)));
