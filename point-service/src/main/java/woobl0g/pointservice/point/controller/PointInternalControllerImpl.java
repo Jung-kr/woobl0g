927ba5dc7307749f69c7de2d86c1d995f59337ea
Jung-kr <jungwy98@gmail.com>
2026-01-11 14:12:16 +0900

    docs(point-service): Swagger API 문서화 인터페이스 추가

---
+++ point-service/src/main/java/woobl0g/pointservice/point/controller/PointInternalControllerImpl.java
@@ -0,0 +1,38 @@
+package woobl0g.pointservice.point.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import woobl0g.pointservice.global.response.ResponseCode;
+import woobl0g.pointservice.point.dto.AddPointRequestDto;
+import woobl0g.pointservice.point.dto.DeductPointRequestDto;
+import woobl0g.pointservice.point.service.PointService;
+
+@RestController
+@RequestMapping("/internal/points")
+@RequiredArgsConstructor
+public class PointInternalControllerImpl implements PointInternalController {
+
+    private final PointService pointService;
+
+    @Override
+    @PostMapping("/add")
+    public ResponseEntity<Void> addPoints(@RequestBody AddPointRequestDto addPointRequestDto) {
+        pointService.addPoints(addPointRequestDto);
+        return ResponseEntity
+                .status(ResponseCode.POINT_ADD_SUCCESS.getStatus())
+                .build();
+    }
+
+    @Override
+    @PostMapping("/deduct")
+    public ResponseEntity<Void> deductPoints(@RequestBody DeductPointRequestDto deductPointRequestDto) {
+        pointService.deductPoints(deductPointRequestDto);
+        return ResponseEntity
+                .status(ResponseCode.POINT_DEDUCT_SUCCESS.getStatus())
+                .build();
+    }
+}
